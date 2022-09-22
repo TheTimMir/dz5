a0bdef5bd27aaf3a2a0f2bcf2cae3e86ad4171ad
Тим <mirocnicencotimofej2008@gmail.com>
2022-09-22 21:22:34 +0300

    Created Gunner class and Dodge func

--- character.py
+++ character.py
@@ -1,3 +1,6 @@
+from random import randint as rint
+
+
 class Character:
     def __init__(self, name='', hp=30, damage=1, armor=0):
         self.name = name
@@ -16,6 +19,10 @@ class Character:
     def take_heal(self, heal):
         # abs - модуль числа (целая часть)
         self.hp = min(self.hp + abs(heal), self.max_hp)
+
+    def dodge(self, target):
+        if rint(0, 7) == 0:
+            target.attack(self)
 
     def stats(self):
         return \
@@ -38,3 +45,28 @@ class Berserk(Character):
         target.take_damage(damage)
         # print(f'{self.name} атаковал {target.name} и нанёс {damage} урона.')
 
+
+class Gunner(Character):
+    def __init__(self, name='', hp=10, damage=5, armor=0, magazineSize=2):
+        Character.__init__(self, name, hp, damage, armor)
+        self.bullets = magazineSize
+        self.magazineSize = magazineSize
+
+    def reload(self):
+        print("choooo")
+        self.bullets = self.magazineSize
+
+    def attack(self, target):
+        if self.bullets > 0:
+            target.take_damage(self.damage)
+            self.bullets -= 1
+        else:
+            print("В магазине закончились патроны. Перезарядите его.")
+
+    def stats(self):
+        return \
+            f' === {self.name} ===\n' \
+            f' Здоровье: {self.hp} / {self.max_hp}\n' \
+            f' Урон: {self.damage}\n' \
+            f' Броня: {self.armor}\n' \
+            f' Патроны: {self.bullets} / {self.magazineSize}'
